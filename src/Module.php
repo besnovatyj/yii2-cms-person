082c117604c7d07196a5d88f6bd367cebd643d83
besnovatyj <metr31@yandex.ru>
2026-06-15 02:55:17 +0500

    to new modman

--- src/Module.php
+++ src/Module.php
@@ -1,42 +1,32 @@
 <?php
 
-
 /*
  * Copyright (c) 2026 Besnovatyj. Licensed under the MIT License.
  */
 
 namespace Besnovatyj\Person;
 
-use common\components\module\BaseModule;
+use common\components\module\CmsModule;
+use modules\modmanNew\contract\DeclaresModule;
+use modules\modmanNew\contract\ProvidesAdminMenu;
+use modules\modmanNew\contract\ProvidesDirectories;
+use modules\modmanNew\contract\ProvidesMigrations;
 use Yii;
 
-class Module extends BaseModule
+class Module extends CmsModule implements
+    DeclaresModule, ProvidesAdminMenu,
+    ProvidesDirectories, ProvidesMigrations
 {
     public const bool EDITABLE = true;
-
-    public static function getAdminMenu(): array
-    {
-        return require __DIR__ . '/config/adminMenu.php';
-    }
-
-    public static function getConfig(): array
-    {
-        return require __DIR__ . '/config/config.php';
-    }
-
-    public static function getOptions(): array
-    {
-        return require __DIR__ . '/config/options.php';
-    }
-
-    public static function getDependencies(): array
-    {
-        return require __DIR__ . '/config/dependencies.php';
-    }
-
-    public static function setContainerConfig()
-    {
-        return (require __DIR__ . '/config/container.php')(\Yii::$container);
-    }
+    public const string VERSION = '1.0.0';
+    public const string MODULE_ID = 'Person';
+    public static function moduleId(): string { return self::MODULE_ID; }
+    public static function moduleVersion(): string { return self::VERSION; }
+    public static function isEditable(): bool { return self::EDITABLE; }
+    public static function adminMenu(): array { return require __DIR__.'/config/adminMenu.php'; }
+    public static function moduleConfig(): array { return require __DIR__.'/config/config.php'; }
+    public static function migrationPath(): string { return __DIR__.'/migrations'; }
+    public static function migrationNamespace(): ?string { return __NAMESPACE__.'\\migrations'; }
+    public static function directories(): array { return ['@static/origin/Person','@static/cache/Person'];}
 
 }
