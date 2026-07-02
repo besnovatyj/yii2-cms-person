f5c6714757d871c98bc495c150e1667812eb1773
besnovatyj <metr31@yandex.ru>
2026-07-02 21:52:17 +0500

    extract modules contracts

--- src/Module.php
+++ src/Module.php
@@ -7,10 +7,10 @@
 namespace Besnovatyj\Person;
 
 use common\components\module\CmsModule;
-use modules\modman\contract\DeclaresModule;
-use modules\modman\contract\ProvidesAdminMenu;
-use modules\modman\contract\ProvidesDirectories;
-use modules\modman\contract\ProvidesMigrations;
+use Besnovatyj\Contracts\module\DeclaresModule;
+use Besnovatyj\Contracts\module\ProvidesAdminMenu;
+use Besnovatyj\Contracts\module\ProvidesDirectories;
+use Besnovatyj\Contracts\module\ProvidesMigrations;
 use Yii;
 
 class Module extends CmsModule implements
